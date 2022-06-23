3e169aa7dd03ff2e4cf5b6e632f0cec89ace108f
hina <iamhinaafzal@gmail.com>
2022-06-23 05:34:14 -0700

    whileloops different examples

---
+++ whileloops.py
@@ -0,0 +1,45 @@
+#whileloop
+#number range
+i=1
+n=5
+while (i<n):
+    print(i)
+    i=i+1
+#first ten odd numbers
+i=1
+n=20
+while (i<n):
+    print(i)
+    i=i+2
+#series 105, 98.....7
+i=105
+n=7
+while(i>=n):
+    print(i)
+    i=i-7
+#first 10 natural numbers in reverse order
+i=10
+n=1
+while (i>=n):
+    print(i)
+    i=i-1
+#sum of first 10 natural numbers
+
+n=10
+sum=0
+while(n>=1):
+    sum = sum+n
+    n = n-1
+    print(sum)
+
+#sum of first 10 even numbers
+
+n=2
+sum=0
+while(n<=20):
+    sum = sum+n
+    n = n+2
+    print (sum)
+
+
+
