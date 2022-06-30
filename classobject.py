8b594cb166282406cb8064766dfd20d9d2db1334
hina <iamhinaafzal@gmail.com>
2022-06-30 08:05:39 -0700

    classes n objects

---
+++ classobject.py
@@ -0,0 +1,10 @@
+class firstclass:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+object1= firstclass("hina", 89)
+object2= firstclass("syed", 88)
+
+print (object1.name)
+print (object2.age)
+
